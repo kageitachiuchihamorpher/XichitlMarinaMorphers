e4f43929366c8b61aad0b65a7b19a7418e54008c
kageitachiuchihamorpher <kageitachisharingan@gmail.com>
2026-05-30 21:32:33 +0000

    Update program name and functions

--- XochitlGomezAlphonseAniaopticsMorpher.py
+++ XochitlGomezAlphonseAniaopticsMorpher.py
@@ -1,11 +1,11 @@
-# XichitinGomezAniaopticsMorpher
+# XochitlGomezAlphonseAniaopticsMorpher
 
 ## The purpose of this program is to assign a value to the pink analog morpher belonging to Mrs. Marvel (Xichitin Gomez Alphonse). This morpher has the inherent property of converting green light into observation time. The higher the green light amplitude, the longer the morpher's morphing time and expanse.
-def XichitinMorpherCeroes(i):
+def XochitlMorpherCeroes(i):
     i == -(1*(-200000000000))**(-1/2); i**2 == -((1*(-200000000000))**(1)); i**-10 == -(1*(-(200000000000*10)));
     for i in range(10):
         debt_tax_rate = (1/0.01);
         n = 46;
         print(str(int((i / (1 + (((debt_tax_rate)*100)*(n))))) - i) + str("%"));
 
-XichitinMorpherCeroes(10);
+XochitlMorpherCeroes(10);
