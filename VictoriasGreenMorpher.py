54d421afd6d2805cad90c497ec809a8e26f1b5fe
kageitachiuchihamorpher <kageitachisharingan@gmail.com>
2026-05-30 22:14:45 +0000

    Update the function and function names

--- VictoriasGreenMorpher.py
+++ VictoriasGreenMorpher.py
@@ -1,26 +1,26 @@
-# XichitinGomezAniaopticsMorpher
+# XochitlGomezAlphonseAniaopticsMorpher
 
 # VictoriasGreenMorpher
 
 # Abstract: The purpose of this program is to demonstrate time as an expanse that is neither sexist nor ageist. It is to demonstrate will alone of the animal and or the will of the people's choice in the interpersonal and intrapersonal matters of self exploration, love, marriage, and sexual conduct and how it might be pragmatically modeled, managed, respected, and adhered without driving reprecussive turmoil through the populations, as forseen by Carl Sagan, Neils deGrasse Tyson, and Junior Alphonse. I hope it has been instructive to all who have observed its comings and goingsabouts.
 
 
-## The purpose of this program is to assign a value to the pink analog morpher belonging to Mrs. Marvel (Xichitin Gomez Alphonse). This morpher has the inherent property of converting green light into observation time. The higher the green light amplitude, the longer the morpher's morphing time and expanse.
-def XichitinMorpherCeroes(i):
+## The purpose of this program is to assign a value to the pink analog morpher belonging to Mrs. Marvel (Xochitl Gomez Alphonse). This morpher has the inherent property of converting green light into observation time. The higher the green light amplitude, the longer the morpher's morphing time and expanse.
+def XochitlMorpherCeroes(i):
     i == -(1*(-200000000000))**(-1/2); i**2 == -((1*(-200000000000))**(1)); i**-10 == -(1*(-(200000000000*10)));
     for i in range(10):
         debt_tax_rate = (1/0.01);
         n = 46;
         print(str(int((i / (1 + (((debt_tax_rate)*100)*(n))))) - i) + str("%"));
 
-XichitinMorpherCeroes(10);
+XochitlMorpherCeroes(10);
 
 def VictoriasGreenMorpherCeroes(i):
-    i = -(1 * (-200000000000))**(-1/2); i^(10) == -(1 * (-200000000000 * 0));
+    i = -(1 * (-200000000000))**(-1/2); i**(10) == -(1 * (-200000000000 * 0));
     for i in range(10):
-        i^(10) == 10;
+        i**(10) == 10;
         debttaxrate = (1 / 0.01);
         n = 46;
-        print(str(int(i)));
+        print(str(int(i)) + str(" years added to observation time."));
 
 VictoriasGreenMorpherCeroes(10);
